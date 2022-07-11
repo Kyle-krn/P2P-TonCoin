a3a6b971e6de6f02ef8c14d5ec5187ec49506436
Egor <egorjkee96@gmail.com>
2022-07-11 21:40:18 +0300

    fix row keyboard

--- keyboards/reply/keyboards.py
+++ keyboards/reply/keyboards.py
@@ -1,10 +1,22 @@
 from aiogram import types
-
 from models import models
 
 
 async def main_keyboard(user: models.User):
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
+
+    keyboard.add(types.KeyboardButton(text="Кошелек" if user.lang == 'ru' else 'Wallet'),
+                 types.KeyboardButton(text="Продать Ton" if user.lang == 'ru' else 'Sell Ton'),
+                 types.KeyboardButton(text="Купить Ton" if user.lang == 'ru' else 'Buy Ton'),
+                 types.KeyboardButton(text="Мои счета" if user.lang == 'ru' else 'My bank account'),
+                 types.KeyboardButton(text="Мои активные заказы" if user.lang == 'ru' else 'My active deals'),
+                 
+                 types.KeyboardButton(text="Реферальная система" if user.lang == 'ru' else "Referal system"),
+                 types.KeyboardButton(text="Схема работы" if user.lang == 'ru' else 'Scheme of work'),
+                 
+                 types.KeyboardButton(text="Язык: ru" if user.lang == 'ru' else 'Language: eng')
+                 )
+
     # text = "Реферальная система" if user.lang == 'ru' else "Referal system"
     ref_button = types.KeyboardButton(text="Реферальная система" if user.lang == 'ru' else "Referal system")
     help_button = types.KeyboardButton(text="Схема работы" if user.lang == 'ru' else 'Scheme of work')
@@ -14,8 +26,4 @@ async def main_keyboard(user: models.User):
     bank_account_button = types.KeyboardButton(text="Мои счета" if user.lang == 'ru' else 'My bank account')
     deals_button = types.KeyboardButton(text="Мои активные заказы" if user.lang == 'ru' else 'My active deals')
     lang_button = types.KeyboardButton(text="Язык: ru" if user.lang == 'ru' else 'Language: eng')
-    keyboard.add(wallet_button, sell_button)
-    keyboard.add(bank_account_button, buy_button)
-    keyboard.add(deals_button, ref_button)
-    keyboard.add(lang_button, help_button)
     return keyboard
