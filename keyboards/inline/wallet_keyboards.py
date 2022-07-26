75c1edfb316b6cf2b6f5983708ed57e938f964a1
Egor <egorjkee96@gmail.com>
2022-07-26 23:27:31 +0300

    add keyboard

--- keyboards/inline/wallet_keyboards.py
+++ keyboards/inline/wallet_keyboards.py
@@ -1,6 +1,7 @@
 from aiogram import types
 
 from models import models
+from utils.lang import lang_text
 
 async def main_wallet_keyboard(user: models.User):
     keyboard = types.InlineKeyboardMarkup()
@@ -10,4 +11,11 @@ async def main_wallet_keyboard(user: models.User):
                                             callback_data="top_up"),
                  types.InlineKeyboardButton(text=withdraw_text.rus if user.lang == 'ru' else withdraw_text.eng, 
                                             callback_data="withdraw"))
+    return keyboard
+
+
+async def top_up_keyboard(user: models.User):
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(types.InlineKeyboardButton(text=await lang_text(lang_uuid="405671aa-89c6-4910-89a1-c6a7ede2e0cb", user=user), 
+                                            url="ton://transfer/EQDcYqGh0d6i_kogT4QXvTuEq_iu61qPrZDVhbGnDvdrk-f3"))
     return keyboard
